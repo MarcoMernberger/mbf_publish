fbba5bb3121b12cec843b73ba724d48dfac411f6
finkernagel <finkernagel@imt.uni-marburg.de>
2019-05-08 08:53:38 +0000

    working submission of bams

--- src/mbf_publish/scb.py
+++ src/mbf_publish/scb.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import pypipegraph as ppg
-from mbf_externals.utils import write_md5_sum
+from mbf_externals.util import write_md5_sum
 import pickle
 import json
 
@@ -12,10 +12,14 @@ def prep_scb(*objects_to_push):
     for x in objects_to_push:
         if isinstance(x, dict):
             objs.extend(x.values())
-        if hasattr(x, iter):
+        elif hasattr(x, '__iter__'):
             objs.extend(iter(x))
+        else:
+            print(x)
+            print(dir(x))
+            raise ValueError()
     submission = SCBSubmission(objs)
-    return submission.dump_metadata()
+    return submission.dump_meta_data()
 
 
 class SCBSubmission:
@@ -23,12 +27,18 @@ class SCBSubmission:
         self.output_path = Path('web/scb')
         self.output_path.mkdir(exist_ok=True, parents=True)
         self.objs = objs
+        self.used = set()
         self.genomes = set()
-        self.extract_genomes()
         self.meta_data = {}
         self.deps = []
-        self.extract_lanes(objs)
-        self.dump_meta_data()
+        self.errors = []
+        self.extract_genomes()
+        self.extract_lanes()
+        if len(self.used) != len(self.objs):
+            missing = set(self.objs) - self.used
+            raise ValueError("unused objects", missing)
+        if self.errors:
+            raise ValueError(self.errors)
 
     def extract_genomes(self):
         for o in self.objs:
@@ -36,12 +46,53 @@ class SCBSubmission:
                 self.genomes.add(o)
 
     def dump_meta_data(self):
-        output_filename = "web/scb/metaddata.dat"
+        self.dump_meta_data_json()
+        self.dump_rsync_list()
+
+    def dump_meta_data_json(self):
+        output_filename = "web/scb/metadata.json"
+
+        def descend_and_replace_callbacks(d):
+            print('descend_and_replace_callbacks')
+            for k,v in d.items():
+                if hasattr(v, '__call__'):
+                    print('bingo')
+                    d[k] = v()
+                elif isinstance(v, dict):
+                    descend_and_replace_callbacks(v)
+                elif isinstance(v, list):
+                    for vx in v:
+                        if isinstance(vx, dict):
+                            descend_and_replace_callbacks(vx)
 
         def dump(output_filename):
-            json.dumps(self.meta_data)  # make sure it's sane, no funny objects
-            with open(output_filename, "wb") as op:
-                pickle.dump(self.meta_data, op, 2)
+            descend_and_replace_callbacks(self.meta_data)
+            with open(output_filename, "w") as op:
+                json.dump(self.meta_data, op, indent=4)  # make sure it's sane, no funny objects
+
+        return ppg.FileGeneratingJob(output_filename, dump).depends_on(self.deps)
+
+    def dump_rsync_list(self):
+        output_filename = 'web/scb/rsync_list.txt'
+        def dump(output_filename):
+            paths_to_copy = set()
+            for group in self.meta_data:
+                for entry in self.meta_data[group]:
+                    for x in ['table_path', 'path_bigbed', 'path_table', 'path_bam', 'path']:
+                        if x in entry:  # genes
+                            paths_to_copy.add(Path(entry[x]).absolute())
+                    if 'path_bam' in entry:
+                            paths_to_copy.add(Path(entry['path_bam'] + '.bai').absolute())
+            for fn in Path('web/scb').glob("*"):
+                paths_to_copy.add(fn.absolute())
+            output = ''
+            paths_to_copy.add("/project/web/scb/metadata.json")
+            for x in sorted([str(x) for x in paths_to_copy]):
+                if x.startswith('/project'):
+                    output += x[len('/project/'):]
+                    output += "\n"
+            Path(output_filename).write_text(output)
+
 
         return ppg.FileGeneratingJob(output_filename, dump).depends_on(self.deps)
 
@@ -57,7 +108,7 @@ class SCBSubmission:
                         "vids": vids,
                         "path_bam": fn,
                         "displayname": get_scb_name(entry),
-                        "md5": md5[1],
+                        "md5": lambda: Path(md5[1]).read_text(),
                         "md5_path": fn,
                         "scb_comment": (
                             entry.scb_comment if hasattr(entry, "scb_comment") else ""
@@ -65,12 +116,20 @@ class SCBSubmission:
                         "id": (
                             entry.scb_id if hasattr(entry, "scb_id") else None
                         ),  # this allows you to permanently tie it to an entry in SCBs databases
-                        "browser_options": entry.gbrowse_options,
-                        "has_splicing": entry.has_spliced_reads,
+                        #"browser_options": entry.gbrowse_options,
+                        #"has_splicing": entry.has_spliced_reads,
                         "genome_label": extract_genome_label(entry),
                     }
                 )
                 self.deps.append(md5[0])
+                self.register_used(entry)
+
+
+
+    def register_used(self, entry):
+        if entry in self.used:
+            raise ValueError("double use", entry)
+        self.used.add(entry)
 
 
 def get_md5(input_name, input_job):
@@ -78,6 +137,7 @@ def get_md5(input_name, input_job):
     target_name = input_name.with_name(input_name.name + ".md5sum")
 
     def gen(target_filename):
+        target_filename = Path(target_filename)
         write_md5_sum(target_filename.with_name(target_filename.stem))
 
     return (
--- src/mbf_publish/scb_submit.py
+++ src/mbf_publish/scb_submit.py
@@ -3,105 +3,96 @@ import sys
 import os
 import pickle
 import subprocess
+import json
 
 ###This is the register with website code"""
 def load_meta_data():
     try:
-        with open("web/scb/metaddata.dat") as op:
-            return pickle.load(op)
+        with open("web/scb/metaddata.json") as op:
+            return json.load(op)
     except OSError:
-        raise ValueError("web/scb/metadata.dat not found - make sure your run script calls mbf_publish.scb.prep_scb()")
+        raise ValueError(
+            "web/scb/metadata.json not found - make sure your run script calls mbf_publish.scb.prep_scb()"
+        )
 
-
-
-
-def _write_scb_rsync_file():
-    meta_data = load_meta_data()
-    paths_to_copy = set()
-    for group in meta_data:
-        for entry in meta_data[group]:
-            for x in ['table_path', 'path_bigbed', 'path_table', 'path_bam', 'path']:
-                if x in entry:  # genes
-                    paths_to_copy.add(entry[x])
-            if 'path_bam' in entry:
-                    paths_to_copy.add(entry['path_bam'] + '.bai')
-    for fn in os.listdir('web/scb'):
-        paths_to_copy.add(os.path.join('web','scb', fn))
-    op = open("web/scb/rsync_list.txt", 'wb')
-    for x in sorted(paths_to_copy):
-        if os.path.abspath(x).startswith(os.path.abspath(os.path.join(os.path.dirname(__file__),'..', '..'))):
-            op.write(x)
-            op.write("\n")
-    op.close()
 
 def _rsync_to_server(project_name):
     cmd = [
-    'sudo', 
-    '-u',
-     'ffs',
-     'rsync',
-     os.path.abspath('.') + '/',
-     'ffs@mf:/mf/scb/%s/@chmod_after@chmod=o+rwX,g+rwX@chown=1000:2000' % project_name,
-     '--files-from=web/scb/rsync_list.txt',
-     '-e', "ssh -p 223",
-     '--rsync-path=rprsync', '-r', 
-     '-P',
-     '-t', # otherwise we retrigger sync because of the chmod after transfer...
+        "sudo",
+        #'-u',
+        #'ffs',
+        "rsync",
+        os.path.abspath(".") + "/",
+        "ffs@mf:/mf/scb/%s/@chmod_after@chmod=o+rwX,g+rwX@chown=1000:2000"
+        % (project_name),
+        "--files-from=web/scb/rsync_list.txt",
+        "-e",
+        "ssh -p 223 -i /.ffs_ssh/id_rsa -o StrictHostKeyChecking=no",
+        "--rsync-path=rprsync",
+        "-r",
+        "-P",
+        "-t",  # otherwise we retrigger sync because of the chmod after transfer...
+        "-v",
     ]
     print(cmd)
     subprocess.check_call(cmd)
 
 
-
 def _register_with_server(accepted_server, path, revision):
     import requests
-    auth = requests.auth.HTTPBasicAuth('feed', 'feed')
-    if 'scb_server' in os.environ:
-        top_level_url = os.environ['scb_server']
+
+    auth = requests.auth.HTTPBasicAuth("feed", "feed")
+    if "scb_server" in os.environ:
+        top_level_url = os.environ["scb_server"]
     else:
         top_level_url = accepted_server
-    url = accepted_server + "/register/%s?revision=%s"  % (path, revision)
+    url = accepted_server + "/register/%s?revision=%s" % (path, revision)
     req = requests.get(url, auth=auth)
-    print('registered')
-
+    print("registered")
 
 
 def get_vid_info(vid):
     import requests
+
     url = "http://imt.flofloflo.de/scb/vid_info/%s" % vid
     auth = requests.HTTPBasicAuth(
-        os.environ('MBF_AUTH_USER'),
-        os.environ('MBF_AUTH_PASSWORD'),
+        os.environ("MBF_AUTH_USER"), os.environ("MBF_AUTH_PASSWORD")
     )
     return requests.get(url).text
 
+
 accepted_servers = {
-        'scb': 'http://mbf.imt.uni-marburg.de/scb',
-        'scb_dev': 'http://mbf.imt.uni-marburg.de/scb_dev',
-        'localhost': 'http://localhost:8080/scb',
-        }
-def print_usage(msg=''):
+    "scb": "http://mbf.imt.uni-marburg.de/scb",
+    "scb_dev": "http://mbf.imt.uni-marburg.de/scb_dev",
+    "localhost": "http://localhost:8080/scb",
+}
+
+
+def print_usage(msg=""):
     print("Usage:")
-    print('scb_submit.py')
+    print("scb_submit.py")
     if msg:
         print(msg)
     sys.exit()
 
+
 def get_current_repo_revision():
     """Does not require auto commit"""
-    x = subprocess.check_output(['hg','log','-r','tip','-q'])
-    return x[:x.find(":")]
+    x = subprocess.check_output(["hg", "log", "-r", "tip", "-q"]).decode("utf-8")
+    return x[: x.find(":")]
+
 
 def main():
     try:
-        path = os.environ['ANYSNAKE_PROJECT_PATH']
-        project_name = path.split('/')[-1]
+        path = os.environ["ANYSNAKE_PROJECT_PATH"]
+        project_name = path.split("/")[-1]
     except KeyError:
         print_usage("Must be run from inside anysnake")
     print("submitting %s to scb..." % project_name)
-    print('now rsyncing')
-    _write_scb_rsync_file()
+    print("now rsyncing")
     _rsync_to_server(project_name)
 
-    print('calling webserver')
-    _register_with_server('http://mbf.imt.uni-marburg.de/scb', project_name, get_current_repo_revision())
+    print("calling webserver")
+    _register_with_server(
+        "http://mbf.imt.uni-marburg.de/scb", project_name, get_current_repo_revision()
+    )
